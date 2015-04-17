cab7c97a0349c064da6abcad294580ccf9f0a386
Collin Walker <ioannes120@gmail.com>
2015-04-16 18:41:16 -0700

    check wifi enhancements

--- app/src/main/java/manitosecurity/ensc40/com/manitosecurity/CheckWifi.java
+++ app/src/main/java/manitosecurity/ensc40/com/manitosecurity/CheckWifi.java
@@ -3,10 +3,13 @@ package manitosecurity.ensc40.com.manitosecurity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Handler;
+import android.preference.PreferenceManager;
 import android.util.Log;
 
 public class CheckWifi extends BroadcastReceiver {
@@ -14,32 +17,50 @@ public class CheckWifi extends BroadcastReceiver {
     private String log = "CheckWifi";
     public Context mcontext;
     public Handler mhandler;
+    private SharedPreferences settings;
+    private String WifiName;
+    private static String lastConnectedSSID;
+
 
     private FeedHandler fh = new FeedHandler(mcontext, mhandler);
 
 
     public void onReceive(final Context context, final Intent intent) {
-        mcontext = context;
+        settings = PreferenceManager.getDefaultSharedPreferences(context);
+        WifiName = settings.getString("WiFiName", "");
+        Log.d(log, "Wifi shared preference: " + WifiName);
+        Log.d(log, "lastConnectedSSID ONRECEIVE----------" + lastConnectedSSID);
+
 
         if(intent.getAction().equals(WifiManager.NETWORK_STATE_CHANGED_ACTION)) {
             NetworkInfo networkInfo =
                     intent.getParcelableExtra(WifiManager.EXTRA_NETWORK_INFO);
+            WifiManager mainWifi = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+            WifiInfo currentWifi = mainWifi.getConnectionInfo();
+            String currentWifiName = currentWifi.getSSID();
+            currentWifiName = currentWifiName.replace("\"", "");
             if(networkInfo.isConnected()) {
+                lastConnectedSSID = currentWifiName;
+                Log.d(log, "new lastConnectedSSID " + lastConnectedSSID);
                 // Wifi is connected
-                Log.d(log, "Wifi is connected: " + String.valueOf(networkInfo));
-                fh.updateFeed("9364467121", "F", "F", "F", "F");
+                if(currentWifiName.equals(WifiName)) {
+                    Log.d(log, "Wifi is connected: " + String.valueOf(networkInfo));
+                    fh.updateFeed("9364467121", "F", "F", "F", "F");
+                }
             }
         } else if(intent.getAction().equals(ConnectivityManager.CONNECTIVITY_ACTION)) {
             NetworkInfo networkInfo =
                     intent.getParcelableExtra(ConnectivityManager.EXTRA_NETWORK_INFO);
             if(networkInfo.getType() == ConnectivityManager.TYPE_WIFI &&
                     ! networkInfo.isConnected() && networkInfo.getState() != NetworkInfo.State.CONNECTING
-                    && networkInfo.getState() != NetworkInfo.State.SUSPENDED) {
+                    && networkInfo.getState() != NetworkInfo.State.SUSPENDED
+                    && lastConnectedSSID.equals(WifiName)) {
                 // Wifi is disconnected
                 Log.d(log, "Wifi is disconnected: " + String.valueOf(networkInfo));
                 fh.updateFeed("9364467121", "T", "F", "T", "F");
             }
         }
     }
+
 
 }
